18ce14095ac412d06566ff9024bcee4905180890
yrajput <yuvrajrathore142@gmail.com>
2025-10-08 20:45:26 +0530

    Height in tailwind

--- 01-basic-css/src/App.jsx
+++ 01-basic-css/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Colors from './components/colors/Colors';
+import Height from './components/height/Height';
 import Margin from './components/margin/Margin';
 import Padding from './components/padding/Padding';
 import Width from './components/width/Width';
@@ -9,7 +10,8 @@ const App = () => {
       {/* <Colors/> */}
       {/* <Padding/> */}
       {/* <Margin/> */}
-      <Width/>
+      {/* <Width/> */}
+      <Height/>
     </div>
   )
 }
---
+++ 01-basic-css/src/components/height/Height.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Height = () => {
+  return (
+    <div>
+        <div className='bg-blue-600 text-white h-20 mb-10'>Height in Tailwind</div>
+        <div className='bg-blue-600  h-[100px] mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quidem blanditiis quo quaerat quod, excepturi officiis impedit dicta similique laboriosam minus, veritatis nulla consequuntur, accusantium numquam ducimus maiores perferendis? Qui tempore exercitationem, voluptate nam quod facilis animi molestiae quam maiores iusto accusamus a dolorem sit facere mollitia eius, soluta adipisci hic vel? Praesentium minus consequuntur qui repudiandae nesciunt error iusto perspiciatis maxime! At aspernatur illo doloribus aliquid fugiat culpa cum incidunt, nesciunt laudantium laborum optio facilis, magnam quas tempore quam sapiente a. Vel soluta possimus est nam obcaecati quasi dolores pariatur dignissimos, esse, sed architecto consequuntur omnis distinctio perferendis. Cumque.</div>
+        <div className='bg-blue-600 text-white min-h-80 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ea in beatae sit, temporibus incidunt. Sunt nostrum veritatis quam ipsam culpa architecto inventore sed, ea deleniti praesentium doloremque! Harum animi fuga ea cupiditate quibusdam fugit dicta neque distinctio reiciendis nesciunt eaque non ipsum facilis adipisci, commodi quaerat hic dolore eius quo doloribus enim assumenda. Inventore, autem sit quibusdam repudiandae quos, aperiam facere corrupti asperiores nesciunt beatae dicta ab quo praesentium porro eaque, nobis magnam repellat id voluptatem est veritatis? Quos optio suscipit modi iste corporis earum magni nesciunt? Quisquam omnis beatae, officia ratione ullam excepturi nihil harum ipsa voluptas recusandae!</div>
+    </div>
+  )
+}
+
+export default Height
