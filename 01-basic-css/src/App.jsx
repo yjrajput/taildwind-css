f2bf941d7e72b1b487bc4b88fd44f43453f2db02
yrajput <yuvrajrathore142@gmail.com>
2025-10-08 19:31:25 +0530

    first commit

---
+++ 01-basic-css/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Colors from './components/colors/Colors';
+const App = () => {
+  return (
+    <div>
+      <Colors/>
+    </div>
+  )
+}
+
+export default App
---
+++ 01-basic-css/src/components/colors/Colors.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Colors = () => {
+  return (
+    <div>
+      
+            <h1 className='font-semibold text-2xl'>Text colors in Taildwind</h1>
+        <p className='text-red-50'>Taildwind colors</p>
+        <p className='text-red-100'>Taildwind colors</p>
+        <p className='text-red-200'>Taildwind colors</p>
+        <p className='text-red-300'>Taildwind colors</p>
+        <p className='text-red-400'>Taildwind colors</p>
+        <p className='text-red-600'>Taildwind colors</p>
+        <p className='text-red-700'>Taildwind colors</p>
+        <p className='text-red-800'>Taildwind colors</p>
+        <p className='text-red-900'>Taildwind colors</p>
+
+        {/* background-color */}
+        <hr />
+        <h1 className='font-semibold text-2xl'>Backgound color in Taildwind</h1>
+        <p className='bg-blue-50'>Backgorund color</p>
+        <p className='bg-blue-100'>Backgorund color</p>
+        <p className='bg-blue-200'>Backgorund color</p>
+        <p className='bg-blue-300'>Backgorund color</p>
+        <p className='bg-blue-400'>Backgorund color</p>
+        <p className='bg-blue-600'>Backgorund color</p>
+        <p className='bg-blue-900'>Backgorund color</p>
+        
+
+        {/* border-color */}
+        <hr />
+        <h1 className='font-semibold text-2xl'>Border color in Taildwind</h1>
+        <p className='border-4 border-green-50'>Border color in Taildwind</p>
+        <p className='border-4 border-green-100'>Border color in Taildwind</p>
+        <p className='border-4 border-green-200'>Border color in Taildwind</p>
+        <p className='border-4 border-green-300'>Border color in Taildwind</p>
+        <p className='border-4 border-green-600'>Border color in Taildwind</p>
+        <p className='border-4 border-green-700'>Border color in Taildwind</p>
+        <p className='border-4 border-green-900'>Border color in Taildwind</p>
+
+        {/* external colors */}
+        <hr />
+        <h1 className='font-semibold text-2xl'>External color in Taildwind</h1>
+        <p className='text-[#bf0a30]'>External color in Taildwind</p>
+        <p className='text-[#d6bc21]'>External color in Taildwind</p>
+        <p className='text-[rgb(11,51,38)]'>External color in Taildwind</p>
+        <p className='text-[hsl(86,75%,60%)]'>External color in Taildwind</p>
+        <p className='text-[hsl(51,73%,48%)]'>External color in Taildwind</p>
+        
+    </div>
+  )
+}
+
+export default Colors
