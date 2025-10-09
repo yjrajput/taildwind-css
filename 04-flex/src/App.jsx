335c6d42d835f848b2920d50d95283db7fe4b2e5
yrajput <yuvrajrathore142@gmail.com>
2025-10-09 14:34:37 +0530

    flex in tailwind

---
+++ 04-flex/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='h-screen'>
+      {/* <div className='bg-amber-700 text-white h-full flex items-center flex-col gap-2 justify-center'>
+        <div className='bg-pink-300 size-40 border-2'>Box-1</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-2</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-3</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        
+      </div> */}
+
+      <div className='bg-amber-700 text-white h-full flex items-center flex-wrap justify-center'>
+        <div className='bg-pink-300 size-40 border-2'>Box-1</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-2</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-3</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        <div className='bg-pink-300 size-40 border-2'>Box-4</div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
