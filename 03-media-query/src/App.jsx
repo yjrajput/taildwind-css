b64d25ac01bff2fe8a378f346c7b0d15c389f97b
yrajput <yuvrajrathore142@gmail.com>
2025-10-09 12:53:42 +0530

    Media query in tailwind

---
+++ 03-media-query/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='h-screen'>
+      <div className='bg-blue-500 h-full lg:bg-red-700 md:bg-blue-500 sm:bg-yellow-600 '></div>
+    </div>
+  )
+}
+
+export default App
