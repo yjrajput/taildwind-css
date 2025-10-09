eb37c1a85c126d9c958372750f081a4c8ae9ff24
yrajput <yuvrajrathore142@gmail.com>
2025-10-09 15:34:41 +0530

    Grid in tailwind

---
+++ 05-grid/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='grid grid-cols-2 grid-rows-2 gap-3  lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1'>
+      <div className='bg-red-300 border-2  border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2  border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2   border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+      <div className='bg-red-300 border-2 border-red-800 h-[100px]'>hello</div>
+    </div>
+  )
+}
+
+export default App
